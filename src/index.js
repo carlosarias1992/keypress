004c0e0176e4ef90553265370610f24cfee6aed3
Carlos <carlosar1219@hotmail.com>
2019-04-10 22:58:56 -0400

    Implement lesson object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import Lesson from './lesson';
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
 
-    const lesson = new Lesson(1);
+    const lesson = new Lesson(0);
     root.appendChild(lesson.render());
 });
