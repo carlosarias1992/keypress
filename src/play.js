51f543c1515b2db94b77dbc0a2a92a9326f4b0b9
Carlos <carlosar1219@hotmail.com>
2019-04-11 16:39:12 -0400

    Add stats to main page

--- src/play.js
+++ src/play.js
@@ -6,39 +6,71 @@ class Play {
     constructor(lessonNumber, parentElement) {
         this.lessonNumber = lessonNumber;
         this.lesson = undefined;
+        this.stats = undefined;
         this.keyboard = undefined;
         this.parentElement = parentElement;
         this.renderKeyboard = this.renderKeyboard.bind(this);
+        this.renderStats = this.renderStats.bind(this);
     }
 
-    renderStats(stats, statsParentElement) {
+    statsElement(parentElement, valueName) {
+        const holder = document.createElement("div");
+        parentElement.appendChild(holder);
+        const title = document.createElement("h2");
+        title.innerHTML = valueName;
+        holder.appendChild(title);
+        const value = document.createElement("p");
+        value.className = valueName.toLowerCase();
+
+        if (valueName === "Accuracy") {
+            value.innerHTML = `${this.stats.accuracy(this.lesson.wrongLetters, this.lesson.editedLetters)}%`;
+        } else if (valueName === "Speed") {
+            value.innerHTML = `${this.stats.speed()} WPM`;
+        }
+
+        holder.appendChild(value);
+    }
+
+    renderStats(statsParentElement) {
         statsParentElement.className = "stats";
-        stats.render();
+
+        this.statsElement(statsParentElement, "Accuracy");
+        this.statsElement(statsParentElement, "Speed");
 
         this.parentElement.appendChild(statsParentElement);
     }
 
-    renderKeyboard(keyboard, keyboardParentElement) {
+    renderKeyboard(keyboardParentElement) {
         keyboardParentElement.className = "keyboard";
-        keyboard.render(this.lesson.letters[0]);
+        this.keyboard.render(this.lesson.letters[0]);
 
         this.parentElement.appendChild(keyboardParentElement);
     }
 
     handleInput() {
-        const { lesson, keyboard } = this;
-        const stats = new Stats(lesson.letters);
+        const { lesson, keyboard, stats } = this;
         const currentLetter = lesson.letters[lesson.currentLetterIndex];
+        const statsSection = document.createElement("div");
 
         document.addEventListener("keypress", e => {
             lesson.handleInput(e);
 
-            if (lesson.currentLetterIndex === 0) {
+            if (lesson.currentLetterIndex === 1) {
                 stats.startTimer();
             }
 
             if (lesson.currentLetterIndex < lesson.letters.length) {
                 keyboard.render(currentLetter);
+            }
+
+            if (lesson.currentLetterIndex === 9) {
+                this.renderStats(statsSection);
+            } else if (lesson.currentLetterIndex > 9) {
+                const accuracy = document.querySelector(".accuracy");
+                accuracy.innerHTML = `${stats.accuracy(this.lesson.wrongLetters, this.lesson.editedLetters)}%`;
+                
+                const speed = document.querySelector(".speed");
+                speed.innerHTML = `${stats.speed()} WPM`;
             }
         });
 
@@ -53,18 +85,16 @@ class Play {
 
     render() {
         this.lesson = new Lesson(this.lessonNumber);
-        const { lesson, parentElement, renderKeyboard, renderStats } = this;
+        const { lesson, parentElement, renderKeyboard } = this;
+
+        this.stats = new Stats(lesson.letters);
 
         parentElement.appendChild(lesson.render());
         lesson.moveCursor();
 
-        const statsSection = document.createElement("div");
-        this.stats = new Stats(lesson.letters);
-        renderStats(stats, statsSection);
-
         const keyboardSection = document.createElement("div");
         this.keyboard = new Keyboard(keyboardSection);
-        renderKeyboard(this.keyboard, keyboardSection);
+        renderKeyboard(keyboardSection);
 
         this.handleInput();
     }
