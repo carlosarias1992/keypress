59869125b6f3d1556907ff15e30a7501f61f2180
Carlos <carlosar1219@hotmail.com>
2019-04-10 23:02:06 -0400

    Update lesson seed file

--- src/lessons.js
+++ src/lessons.js
@@ -1,6 +1,6 @@
 export default {
     0: {
-        lessonName: "Hello World", 
-        content: "Hello World!"
+        lessonName: "Lorem Ipsum", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo dui, lobortis vitae nibh vitae, tempor bibendum nisi. Maecenas tempor feugiat dignissim. Cras placerat enim at mi consequat, ac eleifend enim viverra."
     }
 };
