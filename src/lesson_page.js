010503aba4cf5a75cfd8567243ac86b718a568f5
Carlos <carlosar1219@hotmail.com>
2019-04-14 14:03:30 -0400

    Cache images for fast loading

--- src/lesson_page.js
+++ src/lesson_page.js
@@ -2,14 +2,38 @@ import Game from './game';
 import Header from './header';
 import AudioController from './audio_controller';
 import HomePage from './home_page';
+import keyUrls from './key_urls';
 
 class LessonPage {
     constructor() {
+        this.cache = [];
         this.audioController = new AudioController("assets/audio/key-press.mp3");
         this.header = new Header(this);
         this.game = new Game(this.header, this.audioController);
         this.render = this.render.bind(this);
         this.renderHome = this.renderHome.bind(this);
+        this.cacheImages = this.cacheImages.bind(this);
+    }
+
+    preloadImages(letters) {
+        for (let i = 0; i < letters.length; i++) {
+            const img = new Image();
+            img.src = `assets/images/keys/${keyUrls[letters[i]]}`;
+        }
+    }
+
+    cacheImages(content) {
+        const distinctLetters = [...new Set(content)];
+        const newLetters = [];
+
+        distinctLetters.forEach(letter => {
+            if (!this.cache.includes(letter)) {
+                newLetters.push(letter);
+                this.cache.push(letter);
+            }
+        });
+
+        this.preloadImages(newLetters);
     }
 
     restartGame() {
@@ -22,6 +46,7 @@ class LessonPage {
     }
 
     render(lesson) {
+        this.cacheImages(lesson.content);
         this.game.render(lesson);
     }
 }
