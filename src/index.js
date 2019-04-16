bc71293534099a1eac322282a1df21485a58d392
Carlos <carlosar1219@hotmail.com>
2019-04-16 11:13:24 -0400

    Cache star images

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import HomePage from './home_page';
 import LessonPage from './lesson_page';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const cachedYellowStar = new Image();
+    cachedYellowStar.src = `assets/images/yellow-star.png`;
+
+    const cachedEmptyStar = new Image();
+    cachedEmptyStar.src = `assets/images/empty-star.png`;
+
     const lesson = new LessonPage();
     const home = new HomePage(lesson);
     home.render();
