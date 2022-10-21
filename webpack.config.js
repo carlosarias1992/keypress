116cd815f9dee3f27f566f2914a88791a95abffc
Carlos <carlosar1219@hotmail.com>
2022-10-21 05:51:41 -0400

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["es2015"] },
+        },
+      },
+      {
+        test: /\.scss/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
     },
-    resolve: {
-        extensions: ['.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { presets: ['es2015'] }
-                }
-            },
-            {
-                test: /\.scss/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devServer: {
-        contentBase: path.join(__dirname),
-        compress: true,
-        port: 3000
-    },
-    devtool: 'eval-source-map'
+    compress: true,
+    port: 3000,
+  },
+  devtool: "eval-source-map",
 };
